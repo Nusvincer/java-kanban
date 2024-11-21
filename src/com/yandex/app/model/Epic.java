42b0731e8ced257d9920222a96d73868b1d0669c
yandere <nusvincer@gmail.com>
2024-11-21 04:16:45 +0400

    удалён неиспользуемый импорт Duration в Epic.java

--- src/com/yandex/app/model/Epic.java
+++ src/com/yandex/app/model/Epic.java
@@ -2,7 +2,6 @@ package com.yandex.app.model;
 
 import com.yandex.app.util.Status;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
